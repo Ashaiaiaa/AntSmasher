53055837127eb0ee86fb8adad22e5c08d1c3528c
Zahira Abshari <155355995+Ashaiaiaa@users.noreply.github.com>
2024-01-14 14:44:55 +0700

    Add files via upload

---
+++ AntSmashed_final/AntSmashed/src/AntMain.java
@@ -0,0 +1,10 @@
+package AntSmashed.src;
+
+public class AntMain {
+    public static <AntFrame> void main(String []args){			
+		
+		AntFrame tabFrame = new AntFrame();
+		tabFrame.setVisible(true);
+	}
+    
+}
